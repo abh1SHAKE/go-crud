bf3e5dfb3b8214d0d7ed7f4b40d58d81c844ddf0
Abhishek Sharma <abhshake17@gmail.com>
2025-06-19 18:58:03 +0530

    define book model with GORM and JSON tags

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -2,20 +2,11 @@ package models
 
 import (
 	"gorm.io/gorm"
-	"github.com/abh1SHAKE/go-crud/pkg/config"
 )
-
-var db *gorm.DB 
 
 type Book struct {
 	gorm.Model
-	Name string `gorm:"" json:"name"`
+	Name string `json:"name"`
 	Author string `json:"author"`
 	Publication string `json:"publication"`
-}
-
-func init() {
-	config.Connect()
-	db = config.GetDB()
-	db.AutoMigrate(&Book{}) // Automatically creates the table if it doesn't exist
 }
